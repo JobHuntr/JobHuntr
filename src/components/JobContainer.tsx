1fa929b2c963c68e295cd0a039637d6e8aebc3fa
Katherine Fry <katgfry@me.com>
2023-10-03 12:24:57 -0400

    added JobCard and NewJob functionality with edit
    Co-authored-by: Nhat Trinh <Nhat-Trinh1@users.noreply.github.com>
    Co-authored-by: shandie231 <shandie231@users.noreply.github.com>
    Co-authored-by: Jeremy Wang <JeremyHcWang@users.noreply.github.com>

--- src/components/JobContainer.tsx
+++ src/components/JobContainer.tsx
@@ -1,15 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from 'react-router-dom';
+import JobCard from './JobCard';
 
+// strech: date or how long since application
+export type Job = {
+  company: string;
+  position: string;
+  salary: number;
+  location: string;
+  description: string;
+}
 
 const JobContainer = () => {
+  const [jobList, setJobList] = useState([]);
+
+  const getList = async () => {
+    // const jobListData: responseData = await axios.get('', {
+    //   withCredentials: true,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   }
+    // })
+    const jobListData = [{company: 'abc', position: 'swe', salary: 10000, location: 'NY', description: 'something'}, 
+    {company: 'abcasda', position: 'swe', salary: 20000, location: 'NY', description: 'somethingdasda'}]
+    setJobList(jobListData);
+  }
+
+  useEffect(() => {getList()}, [])
+
+  const listOfCards: JSX.Element[] = [];
+
+  for (let i=0; i<jobList.length; i++) {
+    const jobInfo = jobList[i] as Job
+    listOfCards.push(<JobCard jobInfo = {jobInfo}/>)
+  }
+
+
   return (
     <div>
       <div>
         <h2>Current Job Applications</h2>
+        <div>
+          {listOfCards}
+        </div>
       </div>
-
-      <Outlet />
     </div>
   )
 
