4c4f1cf1e27f91da548d0b7cd5c5c08086e37d65
Michael Shand <shandie231@gmail.com>
2023-10-03 15:04:19 -0400

    add verifyToken middleware and route to backend to skip loggin

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -12,17 +12,39 @@ const LoginPage = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [signup, setSignup] = useState<boolean>(false);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>();
+
+
+
+  const checkLogInStatus = async() => {
+    try{
+    const response: responseData = await axios.get('/user/isLoggedIn', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      withCredentials: true
+    })
+    console.log('response: ', response);
+    //setIsLoggedIn(response.data);
+    return;
+  }catch(err){
+    console.log('Error getting Login Status');
+  }
+  }
+
 
   const clickLogin = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
+      console.log("in try clause")
       const response: responseData = await axios.post('/user/login', {username: username, password: password}, {
         withCredentials: true,
         headers: {
           'Content-Type': 'application/json',
         }
       })
+      console.log("response", response)
       // expect a boolean value
     }
     catch (error){
@@ -68,7 +90,7 @@ const LoginPage = () => {
       {signup && (
       <div id='signup'>
         <h2> Signup </h2>
-        <form onSubmit={(e) => clickSignup(e)}>
+        <form onSubmit={(e) => {clickSignup(e)}}>
           <input type="text" placeholder="Username" value={username} onChange= {(e) => setUsername(e.target.value)}></input>
           <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
           <button>Submit</button>
